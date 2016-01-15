fa62dc4512a081b9585bbf46f7640a2594016504
Gage <gagesnewman@gmail.com>
2016-01-15 15:21:10 -0800

    updated search capabilities to all four text columns

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,8 @@
 class Recipe < ActiveRecord::Base
 
   def self.search(query)
-    Recipe.where(name: query)
+    Recipe.where("name LIKE ? or url LIKE ? or tags LIKE ? or notes LIKE ?",
+    "%#{query}%","%#{query}%","%#{query}%","%#{query}%")
   end
 
 end
